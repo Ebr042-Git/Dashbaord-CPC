b36ab8dede2c0326b210a137d09840dea54d0e7e
Ebr042-Git <ebrahimmohammed568@gmail.com>
2026-01-30 17:30:47 +0400

    Refactor fetch to new URL and improve error handling
    Updated fetch call to use a new URL and modified response handling to log status and display data in taskContainer.

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-const sheetURL = "https://script.google.com/macros/s/AKfycbyRvvLmZg1zzfGsgFb8XVUApclToxCH1ZyqVG0jlWntK94V_TZWcXq_j-uL-WGLtpK6Gw/exec";
+const url = "https://script.google.com/macros/s/AKfycbyRvvLmZg1zzfGsgFb8XVUApclToxCH1ZyqVG0jlWntK94V_TZWcXq_j-uL-WGLtpK6Gw/exec";
 
-fetch(sheetURL)
-  .then(res => res.json())
-  .then(data => {
-    const taskContainer = document.getElementById("taskContainer");
-    taskContainer.innerHTML = "";
-
-    let labels = [];
-    let progressValues = [];
-
-    data.forEach(item => {
-      const task = item["Task"];
-      const percent = Number(item["Completed %"]) || 0;
-
-      labels.push(task);
-      progressValues.push(percent);
-
-      taskContainer.innerHTML += `
-        <div>
-          <h3>${task}</h3>
-          <div class="progress-bar">
-            <div class="progress-fill" style="width:${percent}%"></div>
-          </div>
-          <p>${percent}% Complete</p>
-        </div>
-      `;
-    });
-
-    new Chart(document.getElementById("progressChart"), {
-      type: "bar",
-      data: {
-        labels: labels,
-        datasets: [{
-          label: "% Complete",
-          data: progressValues
-        }]
-      }
-    });
+fetch(url)
+  .then(res => {
+    console.log("Response status:", res.status);
+    return res.json();
   })
-  .catch(err => console.error("Fetch error:", err));
+  .then(data => {
+    console.log("DATA:", data);
+    document.getElementById("taskContainer").innerText =
+      JSON.stringify(data, null, 2);
+  })
+  .catch(err => {
+    console.error("FETCH ERROR:", err);
+  });
